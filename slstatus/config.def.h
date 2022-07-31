b23d2da311cfd92edf0567e9a215b8dd5be3ce18
Alexis Jhon Gaspar <76678069+Lucas-mother3@users.noreply.github.com>
2022-07-31 18:54:21 +0800

    slstatus with new icons!

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -65,7 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ disk_perc, ": %s%%   ", "/"},
-	{ ram_perc, " %s%%   ", NULL},
-	{ datetime, "%s  ",           "%m/%d/%Y %I:%M%P" },
+	{ disk_perc, ":%s%% |", "/"},
+	{ ram_perc, " :%s%% |", NULL},
+	{ cpu_perc, " :%s%% |", NULL},
+	{ datetime, " %s  ", "%m/%d/%y %I:%M%P" },
 };
