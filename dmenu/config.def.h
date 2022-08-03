e2c73a3506d6ed602cd65072de0659c5ebb56596
Alexis Jhon Gaspar <76678069+Lucas-mother3@users.noreply.github.com>
2022-08-04 01:35:47 +0800

    Change font size

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,8 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus-font:size=14:"
-	"font-awesome:size=14:"	
+	"terminus-font:size=12:"
+	"font-awesome:size=12:"	
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
