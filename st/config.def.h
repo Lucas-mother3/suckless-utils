e185c5a88939c265b8e7bcdb6d15c6d8b6aa5d6b
Alexis Jhon Gaspar <76678069+Lucas-mother3@users.noreply.github.com>
2022-08-04 01:36:58 +0800

    Change font size

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "terminus-font:pixelsize=14:antialias=true:autohint=true:font-awesome:pixelsize=14:antialias=true:autohint=true";
+static char *font = "terminus-font:pixelsize=12:antialias=true:autohint=true:font-awesome:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
