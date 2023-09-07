5b62e66c184430f3a8a9305b3c90f5e3511fa6be
Alexis Jhon Gaspar <lucas0021a@outlook.com>
2023-09-07 19:33:55 +0800

    Fix

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -156,7 +156,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ Mod1Mask,             		XK_Tab,    altTabStart,	   {0} },
+	{ MODKEY,             		XK_Tab,    altTabStart,	   {0} },
 	{ MODKEY,                       XK_n,     shiftview,      { .i = +1 } },
 	{ MODKEY,                       XK_b,     shiftview,      { .i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
