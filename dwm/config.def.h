2846db3f0b4f5fb055d5975398a27cd29ebc7b93
Alexis Jhon Gaspar <lucas0021a@outlook.com>
2023-09-09 19:24:04 +0800

    Add spmenu file manager util

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -103,6 +103,7 @@ static Key keys[] = {
 	{ Mod4Mask,                     XK_s,      spawn,          SHCMD("spmenu_run -d -a '-g 4 -l 10'") },
 	{ Mod4Mask|ShiftMask,		XK_s,      spawn,          SHCMD("spmenu_run -a '-g 4 -l 10'") },
 	{ Mod4Mask|Mod1Mask,		XK_s,	   spawn,	   SHCMD("dmenu_run -fn 'Hack Nerd Font:size=14' -nb '#173f4f' -nf '#ffffff' -sb '#124f5f' -sf '#eeeeee' -g 6 -l 6") },
+	{ Mod4Mask,                     XK_f,      spawn,          SHCMD("spmenu_run -fm -a '-g 4 -l 10'") },
 	/* needs spmenu */
 	{ Mod4Mask,			XK_v,	   spawn,	   SHCMD("clipmenu-spmenu") },
 	{ Mod4Mask|ShiftMask,		XK_Print,  spawn,	   SHCMD("screenshot-spmenu -f") },
