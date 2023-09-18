d477fba5241faceb125ac23712530117dda1ff2a
Alexis Jhon Gaspar <lucas0021a@outlook.com>
2023-09-18 19:13:46 +0800

    Add user fetching script

--- dwmblocks-async/config.c
+++ dwmblocks-async/config.c
@@ -10,6 +10,7 @@ Block blocks[] = {
     {"sb-record",  0,    4 },
     {"sb-volume",  1,    5 },
     {"sb-date",    1,    6 },
+    {"sb-user",    0,    7 },
 };
 
 const unsigned short blockCount = LEN(blocks);
