92dc97db10ed0d8b52941878f8bf8e5ef93ff9a9
Alexis Jhon Gaspar <lucas0021a@outlook.com>
2023-09-22 21:34:04 +0800

    Fix forecast

--- dwmblocks-async/config.c
+++ dwmblocks-async/config.c
@@ -4,13 +4,14 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-disk",    1800, 1 },
-    {"sb-memory",  10,   2 },
-    {"sb-loadavg", 5,    3 },
-    {"sb-music",   1,    4 },
-    {"sb-volume",  1,    5 },
-    {"sb-date",    1,    6 },
-    {"sb-user",    0,    7 },
+    {"sb-forecast",  900,  1 },
+    {"sb-disk",      1800, 2 },
+    {"sb-memory",    10,   3 },
+    {"sb-loadavg",   5,    4 },
+    {"sb-music",     1,    5 },
+    {"sb-volume",    1,    6 },
+    {"sb-date",      1,    7 },
+    {"sb-user",      0,    8 },
 };
 
 const unsigned short blockCount = LEN(blocks);
